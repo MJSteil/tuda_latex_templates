b9c64e45ce2aba782cdab3f49be8dc335fc58972
Martin Steil <martinjsteil@gmail.com>
2023-08-03 04:54:17 +0200

    Added prepare_tudaNoPDFs.sh

---
+++ prepare_tudaNoPDFs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+./addlicense.sh
+
+rm -f tuda_templates.zip
+mkdir -p texmf
+rm -rf texmf/*
+
+mkdir -p texmf/doc/latex/tuda-ci
+mkdir -p texmf/tex/latex/tuda-ci
+
+cp -r tex/. texmf/tex/latex/tuda-ci/.
+cp tuda_logo.pdf texmf/tex/latex/tuda-ci/.
+
+mkdir -p texmf/doc/latex/tuda-ci/example
+cp example/*.tex texmf/doc/latex/tuda-ci/example/.
+cp example/*.lco texmf/doc/latex/tuda-ci/example/.
+cp example/*.bib texmf/doc/latex/tuda-ci/example/.
+
+cp README.md texmf/doc/latex/tuda-ci/.
+
+cd texmf
+
+zip -ll -y -r tuda_templates.zip doc tex
+
+mv tuda_templates.zip ../.
