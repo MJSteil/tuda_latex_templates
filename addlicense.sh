d8a5e9f9fbdc31ecbe4166227233eb4581fd05b0
Martin Steil <martinjsteil@gmail.com>
2023-08-03 07:38:14 +0200

    ignore texmf, add license in texmf

--- addlicense.sh
+++ addlicense.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-cd tex
 fileversion="3.30"
 filedate=$(date '+%Y/%m/%d')
+
+cd texmf/tex/latex/tuda-ci
 echo $filedate
 for filename in *.sty *.cls *.def *.cfg *paper.clo
 do
     echo $filename
-    (sed  "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../LICENSE_info && cat $filename) >> tmp_license.txt
+    (sed  "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../../../../LICENSE_info && cat $filename) >> tmp_license.txt
     mv tmp_license.txt $filename
+	
+	echo $filename
+    (sed  "s/<FILENAME>/$(basename $filename)/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../../../../FILEINFO_header && cat $filename) >> tmp_license.txt
+    mv tmp_license.txt $filename
+	
 done
-cd  ..
+cd  ../../../../
 
-cd example
+cd texmf/doc/latex/tuda-ci/example
 for filename in *.tex *.bib *.lco
 do
     echo $filename
-    (cat ../LICENSE_info-demo && cat $filename ) >> tmp_license.txt
+    (cat ../../../../../LICENSE_info-demo && cat $filename ) >> tmp_license.txt
     mv tmp_license.txt $filename
-done
-cd ..
-
-
-for filename in {tex/*.{sty,cls,def,cfg,clo},example/*.{tex,bib,lco}}
-do
-    echo $filename
-    (sed  "s/<FILENAME>/$(basename $filename)/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" FILEINFO_header && cat $filename) >> tmp_license.txt
+	
+	echo $filename
+    (sed  "s/<FILENAME>/$(basename $filename)/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../../../../../FILEINFO_header && cat $filename) >> tmp_license.txt
     mv tmp_license.txt $filename
+	
 done
+cd ../../../../../
+rm tmp_license.txt
--- prepare_tuda.sh
+++ prepare_tuda.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
-./addlicense.sh
-
 rm -f tuda_templates.zip
 mkdir -p texmf
 rm -rf texmf/*
 
 mkdir -p texmf/doc/latex/tuda-ci
-mkdir -p texmf/tex/latex/tuda-ci
 cd example
 latexmk --lualatex
 mv DEMO-*.pdf ../texmf/doc/latex/tuda-ci
 cd ..
 
+mkdir -p texmf/tex/latex/tuda-ci
 cp -r tex/. texmf/tex/latex/tuda-ci/.
 cp tuda_logo.pdf texmf/tex/latex/tuda-ci/.
 
@@ -23,8 +20,9 @@ cp example/*.bib texmf/doc/latex/tuda-ci/example/.
 
 cp README.md texmf/doc/latex/tuda-ci/.
 
-cd texmf
+./addlicense.sh
 
+cd texmf
 zip -ll -y -r tuda_templates.zip doc tex
 
 mv tuda_templates.zip ../.
